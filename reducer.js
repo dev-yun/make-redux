c73d948e9a8a0d657129e6b66ef2f35784debc9b
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-08-03 19:00:15 +0900

    Update : Redux의 한계 (비동기 데이터를 받아오지 못함)
    Redux는 상태를 처리하는 Reducer함수가 순수함수이길 원하는데
    
    Reducer 내에 상태처리를 비동기적으로 작성하면 Reducer은 더이상 순수함수일 수 없어 Reducer에는 비동기코드를 작성할 수 없다.
    
    때문에 미들웨어 아키텍처로 비동기코드는 따로 처리해야한다.

--- reducer.js
+++ reducer.js
@@ -6,12 +6,23 @@
 const InitializeState = { count : 0 }
 export function reducer(state = InitializeState , action) {
     
+    // 상태를 입력받았을때 만약 상태 처리를 비동기적으로 한다면 Reducer의 구조상 비동기 호출이 끝나기전에 return되어 undefined가 반환된다.
+    // 때문에 reducer에서 비동기를 처리하지 않고 다른 방법을 사용한다. (미들웨어 아키텍처를 만들어 reducer 외부에서 해결한다.)
+    // redux는 reducer가 순수함수이길 바라기 때문이다. (순수함수란 입력이 같으면 항상 같은 출력을 내보내는 함수이고 이를 위해선 외부에 의존성이 있으면 안된다. (비동기는 성공할때도, 실패할때도 있으므로 같은 입력이라도 출력이 달라질 수 있다.))
     switch(action.type){
       case 'increase':
         return { ...state, count : state.count + 1 };
       case 'decrease':
         return { ...state, count : state.count - 1 };
       case 'reset':
+        // reset을 서버로부터 비동기적으로 받아와 reset할지 결정하는 함수를 reducer내부에 사용할 수 없다. (비동기 코드이기 때문에)
+        // fetch('/reset')
+        //   .then(response => response.json())
+        //   .then(result => {
+        //     if(result) {
+        //       return { ...state, count : 0 }; 
+        //     }
+        //   })
         return { ...state, count : 0 };
       default:
         return { ...state };  
