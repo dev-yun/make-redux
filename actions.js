86841fae3daa2eb88850d32dbed75c2a65f95b0d
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-08-04 20:09:21 +0900

    Update : middleware에서 비동기 처리 코드 추가
    reducer에서 직접 처리하지 못하는 비동기 처리를 middleware에서 처리하고 값만 reducer에 보내는 방식으로 작동한다.
    
    비동기처리를 위한 객체를 생성하고 curry에 jobs(비동기 작업들)을 추가하여 비동기 작업이 있으면 처리하고 없으면 다음으로 action을 넘기며 실행된다.

--- actions.js
+++ actions.js
@@ -5,5 +5,8 @@ import * as Actions from "./action-types.js";
 // curry 된 부분에서 type을 입력하여 payload를 입력받는 함수를 반환한다.
 export const increase = actionCreator(Actions.INCREASE_COUNTER);
 export const asyncIncrease = actionCreator(Actions.ASYNC_INCREASE_COUNTER);
+export const asyncDecrease = actionCreator(Actions.ASYNC_DECREASE_COUNTER);
 export const decrease = actionCreator(Actions.DECREASE_COUNTER);
 export const setCounter = actionCreator(Actions.SET_COUNTER);
+export const asyncRequest = actionCreator(Actions.ASYNC_REQUEST);
+export const asyncResponse = actionCreator(Actions.ASYNC_RESPONSE);
--- app.js
+++ app.js
@@ -2,20 +2,52 @@ import { createStore } from './redux.js';
 import  reducer  from './reducer.js';
 import * as Actions from './actions.js';
 import { logger } from './logger.js';
+import { ASYNC_DECREASE_COUNTER, ASYNC_INCREASE_COUNTER } from './action-types.js';
 
+const asyncRouter = jobs => store => next => action => {
+    const matchJob = Object.entries(jobs).find(([type]) => action.type === type);
 
-const store = createStore(reducer, [logger]);
+    if(matchJob){
+        matchJob[1](store, action);
+    } else {
+        next(action)
+    }
+}
+
+const asyncJobs = {
+    [ASYNC_INCREASE_COUNTER]: function (store, action) {
+        store.dispatch(Actions.asyncRequest());
+        setTimeout(() => {
+            store.dispatch(Actions.increase(20));
+            store.dispatch(Actions.asyncResponse());
+        }, 3000)
+        
+    },
+    [ASYNC_DECREASE_COUNTER]: function (store, action) {
+        store.dispatch(Actions.asyncRequest());
+        setTimeout(() => {
+            store.dispatch(Actions.decrease(40));
+            store.dispatch(Actions.asyncResponse());
+        }, 3000)
+        
+    },
+}
+
+const store = createStore(reducer, [logger, asyncRouter(asyncJobs)]);
 
 const counterDisplay = document.querySelector('#counter');
+const loadingMessage = document.querySelector('#loading');
 const btnIncrease = document.querySelector('#btn-increase');
 const btnAsyncIncrease = document.querySelector('#btn-async-increase');
+const btnAsyncDecrease = document.querySelector('#btn-async-decrease');
 const btnDecrease = document.querySelector('#btn-decrease');
 const btnReset = document.querySelector('#btn-reset');
 
 // 구독 발행 모델 호출
 store.subscribe(function() {
-  const { counter } = store.getState();
+  const { counter, request } = store.getState();
 
+  loadingMessage.style.visibility = request ? 'visible' : "hidden";
   counterDisplay.textContent = counter;
 });
 
@@ -36,3 +68,7 @@ btnAsyncIncrease.addEventListener('click', () => {
 btnDecrease.addEventListener('click', () => {
   store.dispatch(Actions.decrease());
 });
+
+btnAsyncDecrease.addEventListener('click', () => {
+    store.dispatch(Actions.asyncDecrease({ url: '/async-decrease' }));
+  });
