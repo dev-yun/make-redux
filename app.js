5b1574a01174695651754da8b1461fa07111f843
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-08-04 17:46:28 +0900

    Update : middleware를 통한 상태관리
    Redux의 데이터(상태) 흐름에 맞춰 action을 입력 받아 실행시키는 middleware을 직접 만들어봤다.
    
    middleware을 사용함으로써 순수함수의 컨셉을 위배하지 않고 비동기 처리를 할 수 있게 되었다.

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 import { createStore } from './redux.js';
 import  reducer  from './reducer.js';
 import * as Actions from './actions.js';
+import { ASYNC_INCREASE_COUNTER, SET_COUNTER } from './action-types.js';
 
-const store = createStore(reducer);
+// middleware는 N개가 존재할 수 있고 redux 데이터 흐름에 따라 action을 받는다.
+// 때문에 action을 받는 것 외에도 action을 넘겨주는 작업을 해야한다. 
+const middleware1 = store => next => action => {
+    console.log("m1 =>", action);
+    next(action);
+}
+
+const middleware2 = store => next => action => {
+    console.log("m2 =>", action);
+    if(action.type === SET_COUNTER){
+        action.payload = 10000;
+    }
+    next(action);
+}
+
+const middleware3 = store => next => action => {
+    console.log("m3 =>", action);
+    if(action.type === ASYNC_INCREASE_COUNTER){
+        setTimeout(() => {
+            next(Actions.increase());
+        }, 1000)
+    }else{
+        next(action)
+    }
+};
+
+const middleware4 = store => next => action => {
+    console.log("m4 =>", action);
+    next(action);
+}
+
+
+const store = createStore(reducer, [middleware1, middleware2, middleware3, middleware4]);
 
 const counterDisplay = document.querySelector('#counter');
 const btnIncrease = document.querySelector('#btn-increase');
--- redux.js
+++ redux.js
@@ -3,14 +3,14 @@
 // curry를 사용함으로써 actionCreator(INCREASE, PAYLOAD)를 줄이고 payload만 인자로 받아 사용할 수 있는 상태가 됨 (redux에서 자주 사용하는 테크닉)
 export const actionCreator = type => payload => ({ type, payload })
 
-// redux에서는 사용자가 상태를 바꾸는 함수를 reducer라고 한다. (worker로 구현한 부분)
-export function createStore(reducer) {
+// middlewares에는 비동기를 처리하는 등 다른 역할을 하는 함수들이 element로 포함된다.
+export function createStore(reducer, middlewares = []) {
     let state;
     // 구독할 함수 목록
     const handlers = [];
     
-    // action을 상태로 보낸다는 의미의 dispath라는 이름을 사용한다. (send로 구현한 부분)
     function dispatch(action) {
+        console.log("reduce => ", action)
         state = reducer(state, action);
         handlers.forEach(handler => handler());
     }
@@ -29,6 +29,22 @@ export function createStore(reducer) {
         getState,
         subscribe,
     };
+
+    middlewares = Array.from(middlewares).reverse();
+    let lastDispatch  = dispatch;
+
+    // 처음 dispatch를 lastDispatch에 넣고 N개의 middlewares를 돌면서 모든 middleware를 redux흐름에 맞춰 실행시킨다.
+    middlewares.forEach(middleware => {
+        lastDispatch = middleware(store)(lastDispatch);
+    })
+
+    // 마지막으로 원본 dispatch에 middleware들의 정보를 담은 lastDispatch를 오버라이트 한다.
+    // 이러한 테크닉을 Monkey patching 이라고 한다. (기존의 함수를 다른 것으로 교체하고 교체된 함수에 다른것을 끼어넣은뒤 기존의 함수에 오버라이트 함)
+    console.log(store.dispatch);
+    
+    store.dispatch = lastDispatch;
+
+    console.log(store.dispatch);
 
     return store;
 }
